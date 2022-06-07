5693dc1f6f6de850b28553735e2f354adcc963bd
jobanpreet <joban52@gmail.com>
2022-06-07 11:40:31 -0500

    add contact section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Info from "./components/Info";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Info />
         <Projects />
+        <Contact />
       </main>
     </div>
   );
